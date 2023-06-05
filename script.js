18d1e39c039f82024699f39468788f6313cdbe18
PriyaSandal <133044444+PriyaSandal@users.noreply.github.com>
2023-06-06 00:01:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+    document.getElementById('myForm').addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent form submission
+
+      // Get values from the form
+      var name = document.getElementById('name').value;
+      var year = document.getElementById('year').value;
+
+      // Construct the query string
+      var queryString = '?name=' + encodeURIComponent(name) + '&year=' + encodeURIComponent(year);
+
+      // Update the URL text
+      var urlElement = document.getElementById('url');
+      urlElement.textContent = 'https://localhost:8080/' + queryString;
+    });
